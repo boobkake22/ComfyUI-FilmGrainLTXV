b3093bf5d6db6103da3094875cb283f2e7f8f42f
boobkake22 <boobkake22@gmail.com>
2026-05-27 01:50:28 -0700

    Namespace error

--- __init__.py
+++ __init__.py
@@ -60,7 +60,7 @@ NODE_CLASS_MAPPINGS = {
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "CG_EditRouter": "Edit Router",
+    "FilmGrainLTXV": "Film Grain LTXV",
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
